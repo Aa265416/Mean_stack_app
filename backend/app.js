c86d61c7f63151521edfda52528449098f43d05f
Arpit Sharma <arpit.sharma@spacestem.com>
2025-01-22 15:38:07 +0530

    adding nodemon & posts api

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next)=> {
-    console.log("First middleware");
-    next();
-})
-
-app.use((req, res, next)=> {
-    res.send("Hello from express")
-})
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: "jahsjashd",
+      title: "First server side post",
+      content: "This is coming from server",
+    },
+    {
+      id: "asdasdas",
+      title: "second server side post",
+      content: "This is coming from server",
+    },
+  ];
+  res.status(200).json({
+    message: "Posts fetched Successfully",
+    posts: posts
+  });
+});
 
 module.exports = app;
